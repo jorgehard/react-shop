b4b4997099c97f786a171d5bf7d31f805fde5eea
Jorge Henrique <jorgehenrique@live.com>
2020-11-11 20:34:20 -0300

    Form test

--- src/pages/ProductsGallery/index.js
+++ src/pages/ProductsGallery/index.js
@@ -6,6 +6,7 @@ import {
   Button,
   Container,
   Typography,
+  TextField,
 } from "@material-ui/core";
 import "./style.css";
 
@@ -23,13 +24,14 @@ function ProductsGallery() {
         <CardContent>
           <Grid container spacing={2}>
             <Grid item xs={3}>
-              <form action="">
-                <select>
-                  <option value="">Gender</option>
-                  <option value="">Text</option>
-                  <option value="">Text</option>
-                  <option value="">Text</option>
-                </select>
+              <form row={true} noValidate autoComplete="off">
+                <TextField id="standard-basic" label="Standard" />
+                <TextField id="filled-basic" label="Filled" variant="filled" />
+                <TextField
+                  id="outlined-basic"
+                  label="Outlined"
+                  variant="outlined"
+                />
               </form>
             </Grid>
             <Grid item xs={3}>
