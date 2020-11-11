c1190c1b9ef4da5708b2a036681c57ba630a4587
Jorge Henrique <jorgehenrique@live.com>
2020-11-11 20:34:08 -0300

    Product grid system

--- src/pages/ProductsGallery/index.js
+++ src/pages/ProductsGallery/index.js
@@ -1,29 +1,57 @@
 import {
   Card,
+  Grid,
   CardContent,
   CardActions,
   Button,
   Container,
   Typography,
 } from "@material-ui/core";
+import "./style.css";
 
 function ProductsGallery() {
   return (
     <Container fixed>
-      <Card>
+      <Card className="card-layout">
         <CardContent>
           <Typography color="textSecondary" gutterBottom>
             Word of the Day
           </Typography>
         </CardContent>
       </Card>
-      <Card>
+      <Card className="card-layout">
         <CardContent>
-          <Typography color="textSecondary" gutterBottom>
-            navegacao
-          </Typography>
+          <Grid container spacing={2}>
+            <Grid item xs={3}>
+              <form action="">
+                <select>
+                  <option value="">Gender</option>
+                  <option value="">Text</option>
+                  <option value="">Text</option>
+                  <option value="">Text</option>
+                </select>
+              </form>
+            </Grid>
+            <Grid item xs={3}>
+              item 1
+            </Grid>
+          </Grid>
         </CardContent>
       </Card>
+      <Grid container spacing={2}>
+        <Grid item xs={3}>
+          <Card className="card-layout">
+            <CardContent>
+              <Typography color="textSecondary" gutterBottom>
+                Word of the Day
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={9}>
+          items
+        </Grid>
+      </Grid>
     </Container>
   );
 }
