1404598ab22a99b39774877d9ce466e2619a7b20
Jorge Henrique <jorgehenrique@live.com>
2020-11-11 21:00:44 -0300

    Product grid finish

--- src/pages/ProductsGallery/index.js
+++ src/pages/ProductsGallery/index.js
@@ -2,40 +2,70 @@ import {
   Card,
   Grid,
   CardContent,
-  CardActions,
-  Button,
   Container,
   Typography,
   TextField,
+  Box,
 } from "@material-ui/core";
 import "./style.css";
+import ImgBanner from "../../assets/product-banner.png";
 
 function ProductsGallery() {
   return (
     <Container fixed>
       <Card className="card-layout">
         <CardContent>
-          <Typography color="textSecondary" gutterBottom>
-            Word of the Day
-          </Typography>
+          <Grid container spacing={2}>
+            <Grid item xs={4}>
+              <img src={ImgBanner} alt="Imagem banner" />
+            </Grid>
+            <Grid item xs={8}>
+              textos
+            </Grid>
+          </Grid>
         </CardContent>
       </Card>
       <Card className="card-layout">
         <CardContent>
           <Grid container spacing={2}>
-            <Grid item xs={3}>
+            <Grid item xs={1} />
+            <Grid item xs={5}>
               <form row={true} noValidate autoComplete="off">
-                <TextField id="standard-basic" label="Standard" />
-                <TextField id="filled-basic" label="Filled" variant="filled" />
-                <TextField
-                  id="outlined-basic"
-                  label="Outlined"
-                  variant="outlined"
-                />
+                <Grid container spacing={2}>
+                  <Grid item xs={4}>
+                    <TextField
+                      id="outlined-basic"
+                      label="Outlined"
+                      variant="outlined"
+                    />
+                  </Grid>
+                  <Grid item xs={4}>
+                    <TextField
+                      id="outlined-basic"
+                      label="Outlined"
+                      variant="outlined"
+                    />
+                  </Grid>
+                  <Grid item xs={4}>
+                    <TextField
+                      id="outlined-basic"
+                      label="Outlined"
+                      variant="outlined"
+                    />
+                  </Grid>
+                </Grid>
               </form>
             </Grid>
             <Grid item xs={3}>
-              item 1
+              <Box display="flex" justifyContent="flex-end">
+                <form noValidate autoComplete="off">
+                  <TextField
+                    id="outlined-basic"
+                    label="Outlined"
+                    variant="outlined"
+                  />
+                </form>
+              </Box>
             </Grid>
           </Grid>
         </CardContent>
@@ -44,9 +74,19 @@ function ProductsGallery() {
         <Grid item xs={3}>
           <Card className="card-layout">
             <CardContent>
-              <Typography color="textSecondary" gutterBottom>
-                Word of the Day
-              </Typography>
+              <Box>
+                <Typography color="#000" gutterBottom>
+                  x Gender: Woman
+                </Typography>
+                <Typography color="#000" gutterBottom>
+                  x Category: Dresses
+                </Typography>
+              </Box>
+              <Box>
+                <Typography variant="h5" color="textSecondary" gutterBottom>
+                  Color
+                </Typography>
+              </Box>
             </CardContent>
           </Card>
         </Grid>
